0020123ed1263f4e46aaccbc49f3e42973dd2361
daksh <bhamradaksh@gmail.com>
2025-04-02 19:19:36 -0400

    commit final 2

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,86 +1,83 @@
-//importing the necessary dependancies
 import express from "express";
 import Blog from "../models/blog.js";
-import {cloudinary} from "../config/config.js";
+import { cloudinary } from "../config/config.js";
 import multer from "multer";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
-// This function will help us to upload an image to cloudinary
-const uploadImageToCloudinary = async (file) => {
-    try 
-    {
+// Cloudinary upload function
+const uploadImageToCloudinary = async (file, res) => {
+    try {
         const result = await cloudinary.uploader.upload(file.buffer, {
             folder: "blogimages",
-            resource_type:"image",
-    });
-    return result.secure_url;
-} catch (err){
-    return res.status(400).json({message: `There was an error uploading the image ${err}`});
-}
-}
+            resource_type: "image",
+        });
+        return result.secure_url;
+    } catch (err) {
+        return res.status(400).json({ message: `Error uploading image: ${err}` });
+    }
+};
 
-// This fetches a blog post
+// Get all blogs
 router.get("/", async (req, res) => {
     let blog = await Blog.find();
     return res.json(blog);
 });
 
-// This fetches a single blog post
+// Get a single blog by ID
 router.get('/:id', async (req, res) => {
     let blog = await Blog.findById(req.params.id);
-    if (!blog){
-        return res.status(404).json({message: "Blog not found"});
+    if (!blog) {
+        return res.status(404).json({ message: "Blog not found" });
     }
     return res.json(blog);
 });
 
-// This creates a post
-router.post('/',upload.single("image"), async (req, res) => {
-    try{
+// Create a new blog
+router.post('/', upload.single("image"), async (req, res) => {
+    try {
         let imageUrl = "";
-        if (req.file){
-            imageUrl = await uploadImageToCloudinary(req.file);
+        if (req.file) {
+            imageUrl = await uploadImageToCloudinary(req.file, res);
         }
-        await Blog.create({...req.body, image: imageUrl});
-        return res.status(201).json({message: 'Yay!! Blog successfully created'});
-    }
-    catch (err){
-        return res.status(400).json({message: `Bad request ${err}`});
+        await Blog.create({ ...req.body, image: imageUrl });
+        return res.status(201).json({ message: 'Blog successfully created' });
+    } catch (err) {
+        return res.status(400).json({ message: `Bad request: ${err}` });
     }
 });
 
-// This is used to update the post
-router.put('/:id',upload.single("image"), async (req, res) => {
-    try{
+// Update a blog
+router.put('/:id', upload.single("image"), async (req, res) => {
+    try {
         let blog = await Blog.findById(req.params.id);
-        if(!blog){
-            return res.status(404).json({message: 'No blog Found'});
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
         }
-        if (req.params.id !== req.body._id){
-            return res.status(400).json({message: `Blog ID could not be matched`});
+        if (req.params.id !== req.body._id) {
+            return res.status(400).json({ message: 'Blog ID mismatch' });
         }
         let imageUrl = blog.image;
-        if (req.file){
-            imageUrl = await uploadImageToCloudinary(req.file);
+        if (req.file) {
+            imageUrl = await uploadImageToCloudinary(req.file, res);
         }
-        await Blog.findByIdAndUpdate(req.params.id,{...req.body, image: imageUrl});
-        return res.status(204).json({message: 'Yay!! Blog updated successfully'});
+        await Blog.findByIdAndUpdate(req.params.id, { ...req.body, image: imageUrl });
+        return res.status(204).json({ message: 'Blog updated successfully' });
+    } catch (err) {
+        return res.status(400).json({ message: `Bad request: ${err}` });
     }
-    catch (err){
-        return res.status(400).json({message: `Bad request ${err}`});
-    }});
+});
 
-// This deletes the blog post
-router.delete(':/id', async (req, res) => {
+// Delete a blog
+router.delete('/:id', async (req, res) => {
     let blog = await Blog.findById(req.params.id);
-    if (!blog){
-        return res.status(404).json({message: 'No blog found'});
+    if (!blog) {
+        return res.status(404).json({ message: 'Blog not found' });
     }
     await Blog.findByIdAndDelete(req.params.id);
-    return res.status(204).json({message: 'Yay!! Blog deleted successfully'});
+    return res.status(204).json({ message: 'Blog deleted successfully' });
 });
 
 export default router;
--- main.js
+++ main.js
@@ -1,54 +1,46 @@
-// Importing the dependancies
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-import bodyParser from "body-parser";
-
-// importing the model and controllers 
-import User from "./models/user.js";
-import Blog from "./models/blog.js";
-import usersController from "./controllers/users.js";
+import { passportConfig, jwtConfig } from "./config/config.js";
 import blogsController from "./controllers/blogs.js";
-
-// importing the functions from config file
-
-import { passportConfig } from "./config/config.js";
-import { jwtConfig } from "./config/config.js";
+import usersController from "./controllers/users.js";
 
 dotenv.config();
 
 const app = express();
 
-// using all the functions from Config file
-
-// Database connection string
+// Database connection
 mongoose.connect(process.env.MONGO_URI, {})
-.then((res) => console.log('Connected to mongoDB successfully'))
-.catch((err) => console.log(`Failed to connect: -${err}`));
+    .then(() => {
+        console.log('Connected to MongoDB successfully');
+        app.listen(process.env.PORT || 5000, () => {
+            console.log("Server is running...");
+        });
+    })
+    .catch((err) => console.log(`Failed to connect: ${err}`));
 
+// Configuring passport and jwt
 passportConfig();
 jwtConfig();
 
-// Other app configurations
-app.use(bodyParser.json());
+// Middleware
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    credentials: true, origin: process.env.CLIENT_URL,
+    credentials: true,
+    origin: process.env.CLIENT_URL,
     methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
-    credential: true }));
+}));
 
-app.use(express.json());
-
-// Connecting the api urls to controllers
+// Routes
 app.use("/api/blogs", blogsController);
 app.use("/api/users", usersController);
+
 app.get("/", (req, res) => {
     res.send("Server is running...");
-  });
-// Starting the app
+});
 
 export default app;
-
